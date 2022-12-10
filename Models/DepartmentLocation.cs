83a3c63fe2491f0c902950fb6f076e5c9a58fd3f
Jason Kubo <kuboj86@gmail.com>
2022-12-10 13:35:54 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        //Composite Primary key and foreign key for Department
+        public int DepartmentID { get; set; }
+        //Composite Primary key and foreign key for Location
+        public int LocationID { get; set; }
+
+        //navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
